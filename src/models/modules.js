e8ab2562f2d5aed22249cca0f17184785139ad43
noraaxzsy <alyahudanis9@gmail.com>
2025-05-30 00:46:12 +0700

    add color data

--- src/models/modules.js
+++ src/models/modules.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 
 const moduleSchema = new mongoose.Schema({
-  id: { type: String, required: true, unique: true }, 
+  id: { type: String, required: true, unique: true },
   title: { type: String, required: true },
   description: String,
   thumbnail: String,
+  color: String,
 });
 
 module.exports = mongoose.model("Module", moduleSchema, "modules");
