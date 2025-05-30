52c196a1f3820bb29676ec39441456a549ae60c7
Puti Narita Adzra <studentubacid@student.ac.id>
2025-05-30 14:47:32 +0700

    videoURL

--- src/handlers/handlerModules.js
+++ src/handlers/handlerModules.js
@@ -63,7 +63,7 @@ const getContentById = async (request, h) => {
 };
 
 const addContent = async (request, h) => {
-  const { id, title, pages, videoURL, nextTopicId, prevTopicId } = request.payload;
+  const { id, title, pages, nextTopicId, prevTopicId } = request.payload;
 
   const existing = await Content.findOne({ id });
   if (existing) {
@@ -74,7 +74,6 @@ const addContent = async (request, h) => {
     id,
     title,
     pages,
-    videoURL,
     nextTopicId,
     prevTopicId
   });
--- src/models/contentData.js
+++ src/models/contentData.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 
 const pageSchema = new mongoose.Schema({
   text: { type: String, required: true },
-  imageURL: { type: String, default: null }
+  imageURL: { type: String, default: null },
+  videoULR: { type: String, default: null }
 });
 
 const contentSchema = new mongoose.Schema({
   id: { type: String, required: true, unique: true },
   title: String,
   pages: [pageSchema],
-  videoURL: String,
   nextTopicId: String,
   prevTopicId: String
 });
